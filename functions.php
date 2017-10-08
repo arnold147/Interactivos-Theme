6cd7b9d4cffa0a3db06f706b53c25f35dd8233ca
arnold147 <arnold.147@hotmail.com>
2017-10-08 12:22:09 -0500

    Reardenado

--- functions.php
+++ functions.php
@@ -1,8 +1,14 @@
 <?php
-	register_nav_menus(array(
-	'menu' => 'Menu Superior', 
-	'menu_footer' => 'Menu Footer' 
-	));
+
+// Registrar los menus en Wordpress
+register_nav_menus(
+	array(
+		'menu' => 'Menu Superior', 
+		'menu_footer' => 'Menu Footer' 
+	)
+);
+
+// Agregar Areas para Widgets al Thema	
 function add_widgets_area() {
 	register_sidebar( array(
         'name'          => __( 'Top 1', 'interactivos' ),
@@ -14,14 +20,22 @@ function add_widgets_area() {
     ) );
 }
 add_action( 'widgets_init', 'add_widgets_area' );
+
+
+// Agregar soporte para logo al Thema
 function interactivos_setup() {
 	add_theme_support( 'custom-logo', array(
 		'flex-width' => true,
 	) );
 }
 add_action( 'after_setup_theme', 'interactivos_setup' );
+
+
+// Agregar soporte para Post Thumbnails 
 add_theme_support( 'post-thumbnails' );
 
+
+// Remover el auto corrector de Tags del editor de contenido y agregarlo con otra prioridad de carga
 remove_filter( 'the_content', 'wpautop' );
 add_filter( 'the_content', 'wpautop' , 13);      
 
@@ -30,27 +44,23 @@ add_filter( 'the_content', 'wpautop' , 13);
 add_theme_support( 'title-tag' );
 
 
-// Agregar archivos css y js del thema 
+// Agregar archivos css y js del thema
+add_action( 'wp_enqueue_scripts', 'interactivos_load_css_js' );
 
-add_action( 'wp_enqueue_scripts', 'prefix_load_css_files' );
-
-function prefix_load_css_files() {   
-   
-   wp_register_script('mis-scripts-js', get_template_directory_uri() .'/js/mis-scripts.js' , array('jquery'), '', true);     
+function interactivos_load_css_js() {   
    
    // Hoja style.css general
 	wp_enqueue_style( 'interactivos-style', get_stylesheet_uri() );
    
-   if(!is_front_page()) {
-		wp_enqueue_style( 'interna-css', get_template_directory_uri() . '/css/internas.css');
-   }
+   // Condicional para cargar estilos paginas interas y excluirla en el home
+   if(!is_front_page()){wp_enqueue_style( 'interna-css', get_template_directory_uri() . '/css/internas.css');}
 	   
 	
 	// Estilos especificos del home
-	if(is_front_page()) {
-		wp_enqueue_style( 'home-css', get_template_directory_uri() . '/css/home.css');
-	}
+	if(is_front_page()) {wp_enqueue_style( 'home-css', get_template_directory_uri() . '/css/home.css');}
 	
+	
+	// Cargar js del Thema
+	wp_register_script('mis-scripts-js', get_template_directory_uri() .'/js/mis-scripts.js' , array('jquery'), '', true);    
 	wp_enqueue_script( 'mis-scripts-js' ); 
 }
-
