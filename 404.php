e69fc2e166650db26f177b14d4047074664bbc67
arnold147 <arnold.147@hotmail.com>
2018-08-07 19:10:13 -0500

    Agregando 404.php

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header();?>
+	<h1 class="titulo-pagina">Página no encontrada</h1>
+<div class="container">
+    <p>Puedes regresar a la portada del sitio y ver mas de nuestro contenido
+    <br><br>
+    <a href="<?php echo get_site_url(); ?>">Ir a la portada</a>
+    </p>
+
+</div>		
+<?php get_footer();?>
